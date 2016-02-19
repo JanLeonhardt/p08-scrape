e5a5130a409ac10971472373b956fc54b7d11410
JanLeonahrdt <janleo@gmail.com>
2016-02-19 09:43:27 +0000

    up

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var fs = require ('fs');
+var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
@@ -32,7 +32,5 @@ app.get('/scrape', function(req, res){
 })
 
 app.listen('8081')
-
 console.log('Magic happens on port 8081');
-
 exports = module.exports = app;
